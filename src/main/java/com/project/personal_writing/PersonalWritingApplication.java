1ad109776db9697635949e5ee8a3cf4c4317e8ec
Hyeonjoon <kingpark02@naver.com>
2024-10-14 00:01:58 +0900

    feat: 첫 커밋

---
+++ src/main/java/com/project/personal_writing/PersonalWritingApplication.java
@@ -0,0 +1,13 @@
+package com.project.personal_writing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWritingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalWritingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/personal_writing/PersonalWritingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.personal_writing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWritingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
