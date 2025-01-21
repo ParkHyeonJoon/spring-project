91e18ae0ac556f0a47aee1f2905518acba13ebd7
Hyeonjoon <kingpark02@naver.com>
2025-01-21 15:26:41 +0900

    feat: MySQL 연동하기

--- netplix-core/core-service/build.gradle.kts
+++ netplix-core/core-service/build.gradle.kts
@@ -2,5 +2,6 @@ dependencies {
     implementation(project(":netplix-core:core-usecase"))
     implementation(project(":netplix-core:core-port"))
     runtimeOnly(project(":netplix-adapters:adapter-http"))
+    runtimeOnly(project(":netplix-adapters:adapter-persistence"))
     implementation("org.springframework:spring-context")
 }
